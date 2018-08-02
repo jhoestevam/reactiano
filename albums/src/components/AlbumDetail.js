aa5850d55eacae9b38db3eb3ea428973d076cab3
jhoestevam <jhonatanm222@gmail.com>
2018-08-01 23:25:21 -0300

    Componente de botão redirecionando para o site da amazon

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, Linking} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({album}) =>{
-    const {title, artist, thumbnail_image, image} = album;
+    const {title, artist, thumbnail_image, image, url} = album;
 
     return (
         <Card>
@@ -27,6 +28,10 @@ const AlbumDetail = ({album}) =>{
                     source={{uri : image}}
                     style={styles.imageStyle}
                 />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>BUY</Button>
             </CardSection>
         </Card>
     );
