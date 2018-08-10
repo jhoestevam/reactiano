d3979275be3193531e613f68c7a225450a4eb9c3
jhoestevam <jhonatanm222@gmail.com>
2018-08-09 23:46:26 -0300

    Login and Log out

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import firebase from 'firebase';
-import {Header} from './components/common';
+import {Button, Header, Spinner} from './components/common';
 import LoginForm from './components/LoginForm';
 
 export default class App extends Component{
+    state = {loggedIn: null};
+
     componentWillMount(){
         firebase.initializeApp({
             apiKey: "AIzaSyDTZSgAZD2qPY423epez785S4ieg__jig8",
@@ -14,13 +16,32 @@ export default class App extends Component{
             storageBucket: "auth-944c0.appspot.com",
             messagingSenderId: "217151043551"
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({loggedIn: true});
+            }else{
+                this.setState({loggedIn: false});
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return <Button>Log out</Button>
+            case false:
+                return <LoginForm />
+            default:
+                return <Spinner size="large"/>
+        }
     }
 
     render(){
         return (
             <View>
                 <Header headerText="Authentiocation"/>
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
--- auth/src/components/common/Spinner.js
+++ auth/src/components/common/Spinner.js
@@ -3,7 +3,7 @@ import {View, ActivityIndicator} from 'react-native';
 
 const Spinner = ({size}) => {
     return(
-        <View style={}>
+        <View style={styles.spinnerStyle}>
             <ActivityIndicator size={size || 'large'}/>
         </View>
     );
