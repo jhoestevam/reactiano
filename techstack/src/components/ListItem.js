2262ea5a418c20b8bf6db3dc669c3f7aab5ecf77
jhoestevam <jhonatanm222@gmail.com>
2018-08-24 22:38:15 -0300

    Expanding description of library

--- techstack/src/components/ListItem.js
+++ techstack/src/components/ListItem.js
@@ -4,6 +4,16 @@ import {CardSection} from "./common";
 import * as actions from '../actions';
 
 class ListItem extends Component{
+    renderDescription() {
+        const {library, expanded} = this.props;
+
+        if (expanded) {
+            return (
+                <Text>{library.description}</Text>
+            );
+        }
+    }
+
     render() {
         const {id, title} = this.props.library;
 
@@ -11,14 +21,18 @@ class ListItem extends Component{
             <TouchableWithoutFeedback onPress={() => this.props.selectedLibrary(id)}>
                 <View>
                     <CardSection>
-                        <Text>
-                            {title}
-                        </Text>
+                        <Text>{title}</Text>
                     </CardSection>
+                    {this.renderDescription()}
                 </View>
             </TouchableWithoutFeedback>
         );
     }
 }
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibrary === ownProps.library.id;
+    return {expanded}
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
